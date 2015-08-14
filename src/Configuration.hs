18580e7490580b7e4c72b9e8e23928ccc1a4acc2
Yves Dubromelle <taeradan@dubronetwork.fr>
2015-08-14 15:01:33 +0200

    ajout de type

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -1,10 +1,15 @@
 module Configuration where
 
 import Data.Matrix
+import Data.Vector
 
 type AHPConfiguration = AHPNode
 
 type AHPNodes = [AHPNode]
+
+type PreferenceMatrix = Matrix Double
+
+type PriorityVector = Vector Double
 
 data AHPNode = AHPTree { name :: String
                        , preferenceMatrix :: Matrix Double
