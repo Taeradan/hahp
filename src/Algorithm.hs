bb5166fb5ae4d2535d2095b629bcb75ec0709a4e
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-10-05 18:26:29 +0200

    fix: compilation issue on mac, provide sortOn implementation as fast fixup

--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -7,6 +7,7 @@ import           Algorithm.Validation
 import           Configuration
 import           Data.List
 import           Data.Maybe
+import           Data.Ord                      ( comparing )
 import           Numeric.LinearAlgebra.HMatrix
 
 initAHP :: AHPTree -> (AHPTree, Bool)
@@ -15,7 +16,21 @@ initAHP ahpTree = (newAHPTree, isTreeValid)
           newAHPTree = computeTreePriorityVectors . computeTreeConsistencies $ ahpTree
 
 rankAlternatives :: [Alternative] -> AHPTree -> [Alternative]
-rankAlternatives alts ahpTree = map fst . sortOn snd $ zip alts ranks
+rankAlternatives alts ahpTree = map fst . sortOn' snd $ zip alts ranks
     where ranks = concat . toLists . fromJust $ alternativesPriority rankedAhpTree
           rankedAhpTree = computeTreeAlternativesPriorities alts ahpTree
 
+-- | Sort a list by comparing the results of a key function applied to each
+-- element.  @sortOn f@ is equivalent to @sortBy . comparing f@, but has the
+-- performance advantage of only evaluating @f@ once for each element in the
+-- input list.  This is called the decorate-sort-undecorate paradigm, or
+-- Schwartzian transform.
+-- see https://hackage.haskell.org/package/base-4.8.1.0/docs/src/Data.OldList.html#sortOn
+--
+-- TODO : Remote this function and use default haskell sortOn.
+-- Note: Compilation issues on mac. sortOn is not provided.
+--
+-- @since 4.8.0.0
+sortOn' :: Ord b => (a -> b) -> [a] -> [a]
+sortOn' f =
+  map snd . sortBy (comparing fst) . map (\x -> let y = f x in y `seq` (y, x))
