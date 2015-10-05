38f29fa31e35dff1fb153f77f5dc1b8370b3beab
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-10-05 18:49:21 +0200

    naming in Main

--- src/Main.hs
+++ src/Main.hs
@@ -20,8 +20,9 @@ main = do
     putStrLn ""
     let configs = [sampleAHPConfig, sampleAHPConfig2, sampleAHPConfig3]
     --mapM_ (putStrLn . showConfigurationSummary . initAHP) configs
-    let (initSampleAHPConfig3, _) = initAHP sampleAHPConfig3
-        toto = rankAlternatives sampleAlternatives3 initSampleAHPConfig3
-    print toto
+    let (initSampleAHPConfig3, validationSampleAHPConfig3) = initAHP sampleAHPConfig3
+        ranking = rankAlternatives sampleAlternatives3 initSampleAHPConfig3
+    --putStrLn . showConfigurationSummary $ (initSampleAHPConfig3, validationSampleAHPConfig3)
+    print ranking
     putStrLn ""
     putStrLn $ showAlternatives sampleAlternatives3
