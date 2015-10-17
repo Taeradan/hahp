7fa20d50fed722349387550fba7f68e32acff6ac
Yves Dubromelle <taeradan@dubronetwork.fr>
2015-10-17 14:03:48 +0200

    code reorganisation in main, random index printing not needed

--- src/Main.hs
+++ src/Main.hs
@@ -10,19 +10,15 @@ import           SampleAHP.Config3
 
 main :: IO ()
 main = do
-    time <- getCurrentTime
     let title = "Test de la librairie HAHP"
         author = "JP P, Y D"
+        configs = [sampleAHPConfig1, sampleAHPConfig2, sampleAHPConfig3]
+        alternatives = [sampleAlternatives1, sampleAlternatives2, sampleAlternatives3]
+        sampleDataSet = zip configs alternatives
+
+    time <- getCurrentTime
     putStrLn $ reportHeader title author time
     putStrLn ""
-    putStrLn "# Valeurs de Random Index selon Alonso & Lamata 2006"
-    putStrLn ""
-    let valeursRI = map randomIndexCalculated [1..15]
-    print valeursRI
-    putStrLn ""
-    let configs = [sampleAHPConfig1, sampleAHPConfig2, sampleAHPConfig3]
-    let alternatives = [sampleAlternatives1, sampleAlternatives2, sampleAlternatives3]
-    let sampleDataSet = zip configs alternatives
     mapM_ (putStrLn . simpleAHPSummary) sampleDataSet
     --mapM_ (putStrLn . showConfigurationSummary . initAHP) configs
     --putStrLn . showConfigurationSummary $ (completeTree, validation)
