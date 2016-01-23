757616820922e41a75aa120c9aeb832cd49303ad
Yves Dubromelle <yves+git@dubronetwork.fr>
2016-01-23 02:28:53 +0100

    error generating embeded in the test functions

--- src/HAHP/Algorithm/Validation.hs
+++ src/HAHP/Algorithm/Validation.hs
@@ -12,105 +12,88 @@ validateInputAHPTree ahpTree = (ahpTree, catMaybes $ concatMap (recursiveValidat
 validateAHPTree :: AHPTree -> (AHPTree, [ValidationError])
 validateAHPTree ahpTree = (ahpTree, catMaybes $ concatMap (recursiveValidator ahpTree) errorsList)
 
-errorsInputList :: [ ((AHPTree -> Bool), (AHPTree -> ValidationError)) ]
-errorsInputList = [ (squareMatrixTest, squareMatrixError)
-                  , (parentSizeMatchChildrenTest, parentSizeMatchChildrenError)
-                  , (unitaryDiagTest, unitaryDiagError)
-                  , (nullDivisionTest, nullDivisionError)
-                  , (positivePreferenceTest, positivePreferenceError)
---                  , (childrenNamesUnicityTest, childrenNamesUnicityError)
+errorsInputList :: [ (AHPTree -> Maybe ValidationError) ]
+errorsInputList = [ squareMatrixTest
+                  , parentSizeMatchChildrenTest
+                  , unitaryDiagTest
+                  , nullDivisionTest
+                  , positivePreferenceTest
                   ]
 
-errorsList :: [ ((AHPTree -> Bool), (AHPTree -> ValidationError)) ]
-errorsList = [ (consistencyTest, consistencyError)
+errorsList :: [ (AHPTree -> Maybe ValidationError) ]
+errorsList = [ consistencyTest
              ]
 
 recursiveValidator :: AHPTree
-                   -> ((AHPTree -> Bool), (AHPTree -> ValidationError))
+                   -> (AHPTree -> Maybe ValidationError)
                    -> [Maybe ValidationError]
-recursiveValidator ahpTree (testFnt, errorFnt) =
+recursiveValidator ahpTree testFnt =
     case ahpTree of
         AHPTree {} -> currentValidity : childrenValidity
-            where currentValidity = if testFnt ahpTree
-                                    then Nothing
-                                    else Just (errorFnt ahpTree)
-                  childrenValidity = concatMap (\ x -> recursiveValidator x (testFnt, errorFnt)) (children ahpTree)
+            where currentValidity = testFnt ahpTree
+                  childrenValidity = concatMap (\ x -> recursiveValidator x testFnt) (children ahpTree)
         AHPLeaf {} -> [Nothing]
 
 -- * Tests implementations
 
 -- ** Consistency test
 
-consistencyTest :: AHPTree -> Bool
-consistencyTest (AHPTree _ _ consistency _ _ _) =
-    case consistency of
-      Nothing -> False
-      Just x -> isMatrixConsistent x validationConsistencyThreshold
-
-consistencyError :: AHPTree -> ValidationError
-consistencyError ahpTree =
+consistencyTest :: AHPTree -> Maybe ValidationError
+consistencyTest ahpTree =
     case (consistencyValue ahpTree) of
-      Nothing -> NotComputedConsistencyError { ahpTree = ahpTree }
-      Just x ->  ConsistencyError { ahpTree = ahpTree
-                                  , consistencyThreshold = validationConsistencyThreshold
-                                  , consistency = x
-                                  }
+      Nothing -> Just NotComputedConsistencyError { ahpTree = ahpTree }
+      Just x -> if isMatrixConsistent x validationConsistencyThreshold
+                   then Nothing
+                   else Just ConsistencyError { ahpTree = ahpTree
+                                              , consistencyThreshold = validationConsistencyThreshold
+                                              , consistency = x
+                                              }
 
 validationConsistencyThreshold = 0.1
 
-isMatrixConsistent :: Double ->  Double -> Bool
+isMatrixConsistent :: Double -> Double -> Bool
 isMatrixConsistent consistency threshold
     | consistency < threshold = True
     | otherwise = False
 
 -- ** Tree structure tests
 
-parentSizeMatchChildrenTest :: AHPTree -> Bool
-parentSizeMatchChildrenTest ahpTree = parentSize == childrenSize
-    where parentSize = rows . preferenceMatrix $ ahpTree
-          childrenSize = length . children $ ahpTree
-
-parentSizeMatchChildrenError :: AHPTree -> ValidationError
-parentSizeMatchChildrenError ahpTree =
-    ParentChildrenSizeMismatchError { ahpTree = ahpTree
-                                    , errorParentSize = parentSize
-                                    , errorChildrenSize = childrenSize
-                                    }
-    where parentSize = rows . preferenceMatrix $ ahpTree
-          childrenSize = length . children $ ahpTree
+parentSizeMatchChildrenTest :: AHPTree -> Maybe ValidationError
+parentSizeMatchChildrenTest ahpTree =
+    if parentSize == childrenSize
+       then Nothing
+       else Just ParentChildrenSizeMismatchError { ahpTree = ahpTree
+                                                 , errorParentSize = parentSize
+                                                 , errorChildrenSize = childrenSize
+                                                 }
+        where parentSize = rows . preferenceMatrix $ ahpTree
+              childrenSize = length . children $ ahpTree
 
 -- ** Matrix properties tests
 
-nullDivisionTest :: AHPTree -> Bool
-nullDivisionTest ahpTree = all (/= 0) matrixvalues
+nullDivisionTest :: AHPTree -> Maybe ValidationError
+nullDivisionTest ahpTree = if all (/= 0) matrixvalues
+                              then Nothing
+                              else Just NullDivisionError {ahpTree = ahpTree}
     where matrixvalues = concat . toLists . preferenceMatrix $ ahpTree
 
-nullDivisionError :: AHPTree -> ValidationError
-nullDivisionError ahpTree = NullDivisionError {ahpTree = ahpTree}
-
-positivePreferenceTest :: AHPTree -> Bool
-positivePreferenceTest ahpTree = all (> 0) matrixvalues
+positivePreferenceTest :: AHPTree -> Maybe ValidationError
+positivePreferenceTest ahpTree = if all (> 0) matrixvalues
+                                    then Nothing
+                                    else Just PositivePreferenceError {ahpTree = ahpTree}
     where matrixvalues = concat . toLists . preferenceMatrix $ ahpTree
 
-positivePreferenceError :: AHPTree -> ValidationError
-positivePreferenceError ahpTree = PositivePreferenceError {ahpTree = ahpTree}
-    where matrixvalues = concat . toLists . preferenceMatrix $ ahpTree
-
-squareMatrixTest :: AHPTree -> Bool
-squareMatrixTest ahpTree = rows matrix == cols matrix
+squareMatrixTest :: AHPTree -> Maybe ValidationError
+squareMatrixTest ahpTree = if rows matrix == cols matrix
+                              then Nothing
+                              else Just SquareMatrixError { ahpTree = ahpTree
+                                                          , errorRows = rows matrix
+                                                          , errorCols = cols matrix
+                                                          }
     where matrix = preferenceMatrix ahpTree
 
-squareMatrixError :: AHPTree -> ValidationError
-squareMatrixError ahpTree =
-    SquareMatrixError { ahpTree = ahpTree
-                      , errorRows = rows matrix
-                      , errorCols = cols matrix
-                      }
-        where matrix = preferenceMatrix ahpTree
-
-unitaryDiagTest :: AHPTree -> Bool
-unitaryDiagTest ahpTree = all (== 1) (toList . takeDiag $ matrix)
+unitaryDiagTest :: AHPTree -> Maybe ValidationError
+unitaryDiagTest ahpTree = if all (== 1) (toList . takeDiag $ matrix)
+                             then Nothing
+                             else Just NotUnitaryDiagError {ahpTree = ahpTree}
     where matrix = preferenceMatrix ahpTree
-
-unitaryDiagError :: AHPTree -> ValidationError
-unitaryDiagError ahpTree = NotUnitaryDiagError {ahpTree = ahpTree}
