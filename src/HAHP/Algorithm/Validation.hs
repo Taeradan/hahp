d39974d9e51de5c3420565b8492c59ae7aa692a5
Yves Dubromelle <yves+git@dubronetwork.fr>
2016-01-30 02:00:46 +0100

    add code from Unique package

--- src/HAHP/Algorithm/Validation.hs
+++ src/HAHP/Algorithm/Validation.hs
@@ -1,6 +1,12 @@
 module HAHP.Algorithm.Validation where
 
 import           Control.Parallel.Strategies
+--import           Data.List.Unique
+import           Data.List
+                 ( sort
+                 , sortBy
+                 , group
+                 )
 import           Data.Maybe
 import           HAHP.Data
 import           Numeric.LinearAlgebra.HMatrix
@@ -130,3 +136,29 @@ unitaryDiagTest ahpTree =
        then Nothing
        else Just NotUnitaryDiagError {ahpTree = ahpTree}
   where diagonalValues = toList . takeDiag . preferenceMatrix $ ahpTree
+
+-- * Unique
+
+-- TO REMOVE
+-- https://hackage.haskell.org/package/Unique-0.4.2/docs/src/Data-List-Unique.html#repeated
+
+sg :: Ord a => [a] -> [[a]]
+sg = group . sort
+
+filterByLength :: Ord a => (Int -> Bool) -> [a] -> [[a]]
+filterByLength p = filter (p . length) . sg
+
+-- | 'repeated' finds only the elements that are present more than once in the list. Example:
+--
+-- > repeated  "foo bar" == "o"
+
+repeated :: Ord a => [a] -> [a]
+repeated = repeatedBy (>1)
+
+-- | The repeatedBy function behaves just like repeated, except it uses a user-supplied equality predicate.
+--
+-- > repeatedBy (>2) "This is the test line" == " eist"
+
+repeatedBy :: Ord a => (Int -> Bool) -> [a] -> [a]
+repeatedBy p = map head . filterByLength p
+
