60ae24ed3b79fa938da1e84710e569073e8d8b09
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2017-01-14 14:22:47 +0100

    simple unit tests on basic configuration Config{1,2,3}.hs

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,16 +1,30 @@
+-- Test framework
 import Test.Tasty
 import Test.Tasty.SmallCheck as SC
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
+-- External dependencies
 import Data.List
 import Data.Ord
+import Numeric.LinearAlgebra.HMatrix
+
+-- HAHP
+import HAHP.Data
+import HAHP.Validation.Alternatives
+import HAHP.Validation.Tree
+import HAHP.Validation.Unique
+
+-- Datasets
+import HAHP.Sample.Config1
+import HAHP.Sample.Config2
+import HAHP.Sample.Config3
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [genericTests, libTests]
---tests = testGroup "Tests" [libTests]
+--tests = testGroup "Tests" [genericTests, libTests]
+tests = testGroup "Tests" [libTests]
 
 -- ----------------------------------------------------------------------------
 
@@ -25,9 +39,49 @@ libQcProps = testGroup "(checked by QuickCheck)"
     [
     ]
 
-libUnitTests = testGroup "Library Unit tests"
-    [
+libUnitTests = testGroup "Library Unit tests" [ libUnitTestsConfig1, libUnitTestsConfig2, libUnitTestsConfig3]
+
+libUnitTestsConfig1 = testGroup "Config1"
+    [ testCase "tree name" $ "Super objective" @=? (name tree)
+    , testCase "preference matrix size" $ 2 @=? (rows $ preferenceMatrix tree)
+    , testCase "preference matrix is square" $ True @=? (cols $ preferenceMatrix tree) == (rows $ preferenceMatrix tree)
+    , testCase "preference matrix value" $ fromLists [ [1,1], [1,1]] @=? preferenceMatrix tree
+    , testCase "indicators count - top level" $ 2 @=? (length $ children tree)
+    , testCase "indicators count - total" $ 2 @=? (length $ children tree)
+    , testCase "decision tree valid" $ True @=? (null $ validateInputAHPTree tree)
+    , testCase "alternatives count" $ 6 @=? length alts
+    , testCase "alternatives are valid" $ True @=? (null $ validateAlternatives (tree, alts) )
     ]
+    where tree = sampleAHPConfig1
+          alts = sampleAlternatives1
+
+libUnitTestsConfig2 = testGroup "Config2"
+    [ testCase "tree name" $ "Become the world's master, Pinky and the Brain" @=? (name tree)
+    , testCase "preference matrix size" $ 3 @=? (rows $ preferenceMatrix tree)
+    , testCase "preference matrix is square" $ True @=? (cols $ preferenceMatrix tree) == (rows $ preferenceMatrix tree)
+    , testCase "preference matrix value" $ fromLists [ [1,0.25,4], [4,1,9], [0.25,0.1111111111111111,1]] @=? preferenceMatrix tree
+    , testCase "indicators count - top level" $ 2 @=? (length $ children tree)
+    , testCase "indicators count - total" $ 5 @=? (length $ children tree)
+    , testCase "decision tree valid" $ True @=? (null $ validateInputAHPTree tree)
+    , testCase "alternatives count" $ 4 @=? length alts
+    , testCase "alternatives are valid" $ True @=? (null $ validateAlternatives (tree, alts) )
+    ]
+    where tree = sampleAHPConfig2
+          alts = sampleAlternatives2
+
+libUnitTestsConfig3 = testGroup "Config3"
+    [ testCase "tree name" $ "Testing the Priority vectors computation" @=? (name tree)
+    , testCase "preference matrix size" $ 4 @=? (rows $ preferenceMatrix tree)
+    , testCase "preference matrix is square" $ True @=? (cols $ preferenceMatrix tree) == (rows $ preferenceMatrix tree)
+    , testCase "preference matrix value" $ fromLists [ [1,1/5,1,3], [5,1,3,5], [1,1/3,1,3], [1/3,1/5,1/3,1]] @=? preferenceMatrix tree
+    , testCase "indicators count - top level" $ 4 @=? (length $ children tree)
+    , testCase "indicators count - total" $ 4 @=? (length $ children tree)
+    , testCase "decision tree valid" $ True @=? (null $ validateInputAHPTree tree)
+    , testCase "alternatives count" $ 3 @=? length alts
+    , testCase "alternatives are valid" $ True @=? (null $ validateAlternatives (tree, alts) )
+    ]
+    where tree = sampleAHPConfig3
+          alts = sampleAlternatives3
 
 -- ----------------------------------------------------------------------------
 
