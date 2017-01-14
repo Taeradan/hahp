9036bdc81ec3e418b3abbd9f89c2dabbc1b17e87
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2017-01-14 17:16:01 +0100

    tasty demonstration tests are defined in specific file

--- tests/tests.hs
+++ tests/tests.hs
@@ -23,11 +23,13 @@ import HAHP.Sample.Config1
 import HAHP.Sample.Config2
 import HAHP.Sample.Config3
 
+import Tasty.Sandbox
+
 main = defaultMain tests
 
 tests :: TestTree
---tests = testGroup "Tests" [genericTests, libTests]
-tests = testGroup "Tests" [libTests]
+tests = testGroup "Tests" [genericTests, libTests]
+--tests = testGroup "Tests" [libTests]
 
 -- ----------------------------------------------------------------------------
 
@@ -152,42 +154,3 @@ libUnitTestsConfig3 = testGroup "Config3"
           ( (dynTree, dynAlts), treeErr, altErr) = simpleAHP (tree, alts)
 
 -- ----------------------------------------------------------------------------
-
-genericTests :: TestTree
-genericTests = testGroup "Generic Tests" [genericProperties, genericUnitTests]
-
-genericProperties :: TestTree
-genericProperties = testGroup "Generic Properties" [genericScProps, genericQcProps]
-
-genericScProps = testGroup "Generic (checked by SmallCheck)"
-  [ SC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
-  , SC.testProperty "Fermat's little theorem" $
-      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
-  -- the following property does not hold
-  , SC.testProperty "Fermat's last theorem" $
-      \x y z n ->
-        (n :: Integer) >= 3 SC.==> x^n + y^n /= (z^n :: Integer)
-  ]
-
-genericQcProps = testGroup "Generic (checked by QuickCheck)"
-  [ QC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
-  , QC.testProperty "Fermat's little theorem" $
-      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
-  -- the following property does not hold
-  , QC.testProperty "Fermat's last theorem" $
-      \x y z n ->
-        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
-  ]
-
-genericUnitTests = testGroup "Generic Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
-  ]
--- ----------------------------------------------------------------------------
-
